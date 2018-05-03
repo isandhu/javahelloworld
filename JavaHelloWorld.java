817ab2fb4a0623c84cba54141dc61f97961f2ec6
Indy Sandhu <isandhu@MBP-ISANDHU-1116.local>
2018-05-03 13:54:19 +0100

    Update 3rd May 2018

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,7 +3,7 @@ public class JavaHelloWorld
 	public static void main (String [] args)
 	{
 	System.out.println("Java Hello World");
-	System.out.println("Hello from Indy");
+	System.out.println("Last change made on 3rd May 2018");
 	}
 }
 
